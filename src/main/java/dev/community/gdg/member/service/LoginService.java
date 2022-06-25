4a3a2b2a771cb3aeff693a090e17789da2061ea7
Haeseong Jeon <seong0428@gmail.com>
2022-06-26 05:01:23 +0900

    feat: 닉네임 자동 생성, uuid 저장

--- src/main/java/dev/community/gdg/member/service/LoginService.java
+++ src/main/java/dev/community/gdg/member/service/LoginService.java
@@ -7,7 +7,12 @@ import dev.community.gdg.member.dto.MemberSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +25,8 @@ public class LoginService {
                 .orElseGet(() -> {
                     MemberCreateResponse memberCreateResponse = memberService.createMember(
                             MemberCreateRequest.builder()
-                                    .nickname("nickname")
+                                    .nickname(createRandomNickname())
+                                    .uuid(uuid)
                                     .tagIds(Collections.emptyList())
                                     .build()
                     );
@@ -31,5 +37,18 @@ public class LoginService {
                             .build();
                 });
         return jwtService.encode(memberSpecification.getId());
+    }
+
+    private String createRandomNickname() {
+        List<String> candidates = Arrays.asList(
+                "쿠글",
+                "양파마켓",
+                "오늘의꿈",
+                "마켓킬러"
+        );
+        int randomIndex = ThreadLocalRandom.current().nextInt(0, candidates.size());
+        String company = candidates.get(randomIndex);
+        String number = String.valueOf(ThreadLocalRandom.current().nextInt(0, 10000));
+        return company + number;
     }
 }
